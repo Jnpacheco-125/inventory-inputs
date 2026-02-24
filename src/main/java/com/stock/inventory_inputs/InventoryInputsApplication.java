39510375906902c62f557d5ad826f2864936078c
Jnpacheco-125 <rm350127@fiap.com.br>
2026-02-24 10:14:37 -0300

    first commit

---
+++ src/main/java/com/stock/inventory_inputs/InventoryInputsApplication.java
@@ -0,0 +1,13 @@
+package com.stock.inventory_inputs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryInputsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryInputsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stock/inventory_inputs/InventoryInputsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock.inventory_inputs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryInputsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
